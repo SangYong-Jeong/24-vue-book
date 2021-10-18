6925ef22e40e9765642418685b4c3c09ac25b15d
SangYong-Jeong <mmaf@naver.com>
2021-10-18 12:59:27 +0900

    Make InfoImg, InfoTxt

---
+++ src/components/mixins/mixininfo.js
@@ -0,0 +1,12 @@
+export default {
+  proprs: ["styled", "title", "content"],
+  computed: {
+    style() {
+      if (this.styled && this.styled.padding) {
+        return `padding: ${this.styled.padding};`;
+      } else {
+        return `padding: 0;`;
+      }
+    },
+  },
+};
