3cac3c59c95033947298398c4566da765f3ea2aa
SangYong-Jeong <mmaf@naver.com>
2021-10-18 10:25:02 +0900

    Intersection Observer 구현중

--- src/components/infinite/InfiniteCp.vue
+++ src/components/infinite/InfiniteCp.vue
@@ -1,10 +1,43 @@
 <template>
-  <h1>Infinite</h1>
+  <section class="wrapper list-wrapper">
+    <TableCp :isPage="false" />
+    <div v-observe="changeVisible" />
+  </section>
 </template>
 
 <script>
+import TableCp from "@/components/common/TableCp";
+import { mapGetters } from "vuex";
+
 export default {
   name: "InfiniteCp",
+  components: { TableCp },
+  data() {
+    return {
+      page: 1,
+      books: [],
+    };
+  },
+  computed: {
+    ...mapGetters(["GET_BOOKS"]),
+  },
+  watch: {
+    GET_BOOKS: function (v) {
+      this.books.push(...v.books);
+    },
+  },
+  created() {
+    this.$store.dispatch("ACT_BOOKS", { page: this.page++, listCnt: 20 });
+  },
+  methods: {
+    changeVisible(isVisible, entry) {
+      console.log(isVisible);
+      console.log(entry);
+      if (isVisible) {
+        this.$store.dispatch("ACT_BOOKS", { page: this.page++, listCnt: 20 });
+      }
+    },
+  },
 };
 </script>
 
